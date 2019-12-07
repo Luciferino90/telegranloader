34694e03b052a29de69956a5cbed005e946f3dd2
Luca Fanciullini <luca.fanciullini@gmail.com>
2019-12-07 16:16:01 +0100

    First commit

---
+++ src/main/java/it/usuratonkachi/telegranloader/telegranloader/TelegranloaderApplication.java
@@ -0,0 +1,13 @@
+package it.usuratonkachi.telegranloader.telegranloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegranloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegranloaderApplication.class, args);
+	}
+
+}
