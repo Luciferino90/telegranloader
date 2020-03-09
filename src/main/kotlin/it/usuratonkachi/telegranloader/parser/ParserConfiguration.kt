793e5feefd4bdfe0fa6ddc8717a9acba1cfc47f9
Luca Fanciullini <luca.fanciullini@gmail.com>
2020-03-10 00:31:23 +0100

    Parser, will be moved in configuration

--- src/main/kotlin/it/usuratonkachi/telegranloader/parser/ParserConfiguration.kt
+++ src/main/kotlin/it/usuratonkachi/telegranloader/parser/ParserConfiguration.kt
@@ -1,32 +1,52 @@
 package it.usuratonkachi.telegranloader.parser
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
-import javax.annotation.PostConstruct
+import org.springframework.stereotype.Component
 
-@ConstructorBinding
+@Component
 @ConfigurationProperties(prefix = "series")
-data class ParserConfiguration (
-        var parser: Map<String, ConfigurationMapper> = mapOf()
-){
-    @PostConstruct
-    fun init(){
-        parser.size
-        println("")
+class ParserConfiguration (
+        var parser: Map<String, ConfigurationMapper>? = null
+)
+
+
+class ConfigurationMapper(
+        var logicalName: String? = null,
+        var type: String? = null,
+        var filename: Element? = null,
+        var season: Element? = null,
+        var episode: Element? = null
+)
+
+class Element(
+        var regexp: String? = null,
+        var fixed: String? = null
+)
+
+// TODO FIX
+fun getEpisodeWrapper(configurationMapper: ConfigurationMapper, mediaName: String) : ParserService.EpisodeWrapper {
+    var cleanMediaName: String = mediaName.replace(".US.", "")
+
+    var filename: String
+    var season: String
+    var episode: String
+
+    val extension = mediaName.split(".").last()
+
+    if (!configurationMapper.filename!!.fixed.isNullOrEmpty()) {
+        filename = configurationMapper.filename!!.fixed!!
+    } else {
+        filename = configurationMapper.filename!!.regexp!!.toRegex().split(cleanMediaName)[0].replace(".", " ").trim()
     }
+    if (!configurationMapper.season!!.fixed.isNullOrEmpty()) {
+        season = configurationMapper.season!!.fixed!!
+    } else {
+        season = configurationMapper.season!!.regexp!!.toRegex().find(cleanMediaName)!!.groupValues.first().removePrefix(configurationMapper.season!!.regexp!!.toRegex().toString()[1].toString())
+    }
+    if (!configurationMapper.episode!!.fixed.isNullOrEmpty()) {
+        episode = configurationMapper.episode!!.fixed!!
+    } else {
+        episode = configurationMapper.episode!!.regexp!!.toRegex().find(mediaName)!!.groupValues.first().removePrefix(configurationMapper.episode!!.regexp!!.toRegex().toString()[1].toString())
+    }
+    return ParserService.EpisodeWrapper(season, episode, filename, extension)
 }
-
-@ConstructorBinding
-data class ConfigurationMapper(
-        var logicalName: String = "",
-        var type: String = "",
-        var filename: Element = Element(),
-        var season: Element = Element(),
-        var episode: Element = Element()
-)
-
-@ConstructorBinding
-data class Element(
-        var regexp: String = "",
-        var fixed: String = ""
-)
